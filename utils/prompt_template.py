ff9aebb5813cff0b9a004f4d7464c6d8b61f3a25
annaTWD <c.datos17@salascala.com>
2024-10-04 16:15:05 +0200

    actualizo

--- utils/prompt_template.py
+++ utils/prompt_template.py
@@ -1,21 +1,12 @@
-from langchain.prompts import PromptTemplate
 
-def get_prompt_template():
-    """
-    Define el prompt para extraer información de una factura o documento.
-    
-    Returns:
-        PromptTemplate: La plantilla del prompt lista para ser utilizada.
-    """
-    prompt_template = PromptTemplate(
-        input_variables=["text"],
-        template="""
-        Por favor, extrae la siguiente información del manual de manera estructurada:
-        
-        Manual:
-        {text}
+def format_prompt(context, query):
+    prompt = f"""
+    Context:
+    {context}
 
-        Respuesta:
-        """
-    )
-    return prompt_template
+    Query:
+    {query}
+
+    Based on the context above, generate a response.
+    """
+    return prompt
