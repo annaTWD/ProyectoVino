8a9f18572ae0adc009fa35fb834b8fe6926314b8
annaTWD <c.datos17@salascala.com>
2024-10-07 12:01:14 +0200

    Eliminando todos los archivos de la rama main

--- models/model1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import faiss
-import numpy as np
-from transformers import AutoTokenizer, AutoModel
-import torch
-
-df = pd.read_csv('../data/skills.csv', sep=';')
-
--- utils/data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_data(filepath):
-    return pd.read_csv(filepath, delimiter=';')
-
-def search_data(df, query):
-    results = df[df.apply(lambda row: row.astype(str).str.contains(query, case=False).any(), axis=1)]
-    return results
